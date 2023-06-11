1259b6cae632ffc9a60182e2668435bc68e46392
HS980924 <choihs980924@gmail.com>
2023-06-12 04:22:40 +0900

    Complete Profile Card func with swiper

--- src/components/ImageCards.js
+++ src/components/ImageCards.js
@@ -1,15 +1,61 @@
+import { Swiper, SwiperSlide } from "swiper/react"; // basic
+import { Navigation, Pagination, A11y, Autoplay, EffectCards } from 'swiper';
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import Carousel from 'react-bootstrap/Carousel';
-
-import '../styles/ImageCard.scss'
+import "swiper/swiper-bundle.min.css";
+import "../styles/ImageCard.scss";
 
 
 const ImageCards = () => {
-    
     // 실제 데이터
-    // const [ allUserInfo, setAllUserInfo ] = useState(null);
+    const userProfile = [
+        {
+            "userId" : 1,
+            "username": "gildong",
+            "profileImgUrl": "/profile/dummy card.png",
+        },
+        {
+            "userId" : 2,
+            "username": "gildong",
+            "profileImgUrl": "/profile/dummy card.png",
+        },
+        {
+            "userId" : 3,
+            "username": "gildong",
+            "profileImgUrl": "/profile/dummy card.png",
+        },
+        {
+            "userId" : 4,
+            "username": "gildong",
+            "profileImgUrl": "/profile/dummy card.png",
+        },
+        {
+            "userId" : 5,
+            "username": "gildong",
+            "profileImgUrl": "/profile/dummy card.png",
+        },
+        {
+            "userId" : 6,
+            "username": "gildong",
+            "profileImgUrl": "/profile/dummy card.png",
+        },
+        ,
+        {
+            "userId" : 7,
+            "username": "gildong",
+            "profileImgUrl": "/profile/dummy card.png",
+        },
+        ,
+        {
+            "userId" : 8,
+            "username": "gildong",
+            "profileImgUrl": "/profile/dummy card.png",
+        }
+
+    ]
+    const [ allUserInfo, setAllUserInfo ] = useState(userProfile);
 
     // const read_AllUserInfo = async()=>{
     //     try{
@@ -25,28 +71,29 @@ const ImageCards = () => {
     //     read_AllUserInfo();
     // },[allUserInfo,]);
 
-    
-
-    return(
+    return (
         <div className="ImageCardBox">
-            <Carousel>
-                <Carousel.Item interval={7000}>
-                    <div className="ImageGroup">
-                        <img className="ImageCard" src="/profile/dummy card.png" alt='img' />
-                        <img className="ImageCard" src="/profile/dummy card.png" alt='img' />
-                        <img className="ImageCard" src="/profile/dummy card.png" alt='img' />
-                        <img className="ImageCard" src="/profile/dummy card.png" alt='img' />
-                    </div>
-                </Carousel.Item>
-                <Carousel.Item interval={7000}>
-                    <div className="ImageGroup">
-                        <img className="ImageCard" src="/profile/dummy card.png" alt='img' />
-                        <img className="ImageCard" src="/profile/dummy card.png" alt='img' />
-                        <img className="ImageCard" src="/profile/dummy card.png" alt='img' />
-                        <img className="ImageCard" src="/profile/dummy card.png" alt='img' />
-                    </div>
-                </Carousel.Item>
-                </Carousel>
+            <Swiper className="SwiperBox"
+                modules={[Navigation, A11y, Autoplay, EffectCards]}
+                slidesPerView={3}
+                loop={true}
+                navigation={true}
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false,
+                    pauseOnMouseEnter: false,
+                    stopOnLastSlide: false,
+                    waitForTransition: true 
+                }}
+            >
+            {
+                allUserInfo.map(user=>
+                    <SwiperSlide className="SwiperSlideBox" key={user.userId}>
+                        <img src={user.profileImgUrl} alt='img'/>
+                    </SwiperSlide>
+                )
+            }
+            </Swiper>
         </div>
     );
 }
