67496c53a70bb2b5b08df9020c6fc5886667289d
HS980924 <choihs980924@gmail.com>
2023-07-02 02:40:59 +0900

    Notice All Page Complete

--- src/components/Notice/NoticeItem.js
+++ src/components/Notice/NoticeItem.js
@@ -1,25 +1,50 @@
+import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from "react-router-dom";
 
 import '../../styles/Notice/NoticeItem.scss';
 
 const NoticeItem = ({noticeItem}) =>{
 
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const [ isClickTitle, setIsClickTitle ] = useState(true);
+    
+
     const onClickItem = (id) => {
-        console.log(id);
+        navigate(`/notice/detail/${id}`);
     }
 
+    const isClickCheck = () => {
+        const path = location.pathname.split('/');
+        path[2] === 'list' ? setIsClickTitle(true) : setIsClickTitle(false);
+    }
+
+    useEffect(()=>{
+        isClickCheck();
+        console.log(isClickTitle);
+    },[]);
+
     return(
-        <div className="RowNoticeItemBox" onClick={()=>onClickItem(noticeItem?.announcementsId)}>
+        <div className="RowNoticeItemBox">
             <div className='NoticeItemNum'>
-                {noticeItem.announcementsId}
+                {noticeItem?.announcementsId}
             </div>
-            <div className='NoticeItemTitle'>
-                {noticeItem.title}
-            </div>
+            {
+                isClickTitle ? 
+                <div className='NoticeItemTitle' onClick={()=>onClickItem(noticeItem?.announcementsId)}>
+                    {noticeItem?.title}
+                </div> :
+                <div className='NoticeItemTitle'>
+                    {noticeItem?.title}
+                </div>
+
+            }
             <div className='NoticeItemCreated'>
-                {noticeItem.createDate}
+                {noticeItem?.createDate}
             </div>
             <div className='NoticeItemWriter'>
-                {noticeItem.writer}
+                {noticeItem?.writer}
             </div>
         </div>
     );
