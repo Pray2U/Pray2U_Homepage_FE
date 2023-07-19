449cd50fc36f569b822da7ab22a31d481b971907
HS980924 <choihs980924@gmail.com>
2023-07-19 10:04:38 +0900

    Editing MyShop Css

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 
 import Template from "../components/Template";
+import MypageHeader from "../components/Header/MypageHeader";
 import MyAttend from "../components/Mypage/MyAttend";
 import MyInfo from "../components/Mypage/MyInfo";
+import MyTil from "../components/Mypage/MyTil";
 
 import '../styles/MyPage/Mypage.scss';
-import MypageHeader from "../components/Header/MypageHeader";
+import MyShop from "../components/Mypage/MyShop";
 
 const Mypage = () =>{
 
@@ -17,7 +19,9 @@ const Mypage = () =>{
 
     const myPageComponent = [
         <MyInfo/>,
-        <MyAttend/>
+        <MyAttend/>,
+        <MyTil/>,
+        <MyShop/>
     ]
 
     return(
@@ -27,7 +31,7 @@ const Mypage = () =>{
                     <div className="MyPageTitle">My Page</div>
                     <div className="MyPageSubTitle">Pray2U Member</div>
                 </div>
-                <div className="MyPageContainer">
+                <div className={selectedMenu < 2 ? "MyPageContainer" : "MyPageOverFlowContainer"}>
                     <MypageHeader onChangeMenu={onChangeMenu} selectedMenu={selectedMenu}/>
                     {
                     myPageComponent?.map((component,idx) =>
