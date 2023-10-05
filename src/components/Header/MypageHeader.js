3cca4ea5f0210db986597691db179b82f7fa1598
HS980924 <choihs980924@gmail.com>
2023-10-05 17:28:33 +0900

    Edit admin paner at admin header menu of Mypage

--- src/components/Header/MypageHeader.js
+++ src/components/Header/MypageHeader.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import '../../styles/Header/MypageHeader.scss';
 import axios from "axios";
 
 const MypageHeader = ({onChangeMenu, selectedMenu}) =>{
 
-    const headers = [
+    const userHeaders = [
         {
             id:'info',
             title:'정보수정',
@@ -46,38 +47,52 @@ const MypageHeader = ({onChangeMenu, selectedMenu}) =>{
         }
     ]
 
-    const [isAdmin, setIsAdmin] = useState(true);
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [headerMenu, setHeaderMenu] = useState(userHeaders);
 
     const read_userRole = async() =>{
         try{
             const url = ''
             const response = await axios.get(url);
             if(response.status === 200){
-                if (response.data.Role === 'ADMIN')
+                if (response.data.Role === 'ADMIN'){
                     setIsAdmin(true);
-                else setIsAdmin(false);
+                    setHeaderMenu([...adminHeaders])
+                }
+                else{
+                    setIsAdmin(false);
+                    setHeaderMenu([...userHeaders])
+                } 
             }
         }catch(e){
             console.log(e);
         }
     }
 
+    const test_read_userRole = () => {
+        if(true){
+            setIsAdmin(true)
+            setHeaderMenu([...adminHeaders])
+        }
+        else{
+            setIsAdmin(false)
+            setHeaderMenu([...userHeaders])
+        }
+    }
+
     useEffect(() => {
         // read_userRole();
+        test_read_userRole();
     }, [])
 
     return(
         <div className="MyPageHeaderBox">
             {
-                isAdmin ? 
-                adminHeaders.map((menu,idx) => 
-                    idx === selectedMenu ? 
-                    <div key={menu.id} className="SelectedMenu" onClick={()=>onChangeMenu(idx)}>{menu?.title}</div> :
-                    <div key={menu.id} className="Menu" onClick={()=>onChangeMenu(idx)}>{menu?.title}</div>):
-                headers.map((menu,idx) =>
-                    idx === selectedMenu ? 
-                    <div key={menu.id} className="SelectedMenu" onClick={()=>onChangeMenu(idx)}>{menu?.title}</div> :
-                    <div key={menu.id} className="Menu" onClick={()=>onChangeMenu(idx)}>{menu?.title}</div>
+                headerMenu.map((menu,idx) =>
+                    menu.id !== 'admin' ?
+                    <div key={menu.id} className={idx === selectedMenu ? "SelectedMenu" : "Menu"} onClick={()=>onChangeMenu(idx)}>{menu?.title}</div>
+                    :
+                    <Link to='/admin' className="Menu">{menu?.title}</Link>
                 )
             }
         </div>
