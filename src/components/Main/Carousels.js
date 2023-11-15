039d30cdf832bfb05204f6cdab02b8fedfc9b9c0
JanBe <ppk1998@naver.com>
2023-11-15 20:08:29 +0900

    mainpage css reset

--- src/components/Main/Carousels.js
+++ src/components/Main/Carousels.js
@@ -1,26 +1,27 @@
-import Carousel from 'react-bootstrap/Carousel';
-import 'bootstrap/dist/css/bootstrap.css'
-import '../../styles/Main/Carousel.scss';
-import { useState } from 'react';
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.css";
+import "../../styles/Main/Carousel.scss";
+import { useState } from "react";
 
 const Carousels = () => {
-    const [ profile, setProfile ] = useState([...Array(3)].map((v,i) => i))
-    
-    return(
-        <div className='CarouselBox'>
-            <Carousel>
-                {profile.map(i=>
-                    <Carousel.Item key={i} interval={4000}>
-                        <img
-                            className="d-block w-100"
-                            style={{ height: "400px", zIndex: 2 }}
-                            src={`/carousel_img/${i+1}.png`}
-                            alt='img'
-                        />
-                    </Carousel.Item>)}
-            </Carousel>
-        </div>
-    );
-}
+  const [profile, setProfile] = useState([...Array(3)].map((v, i) => i));
+
+  return (
+    <div className="w-full h-[55%] pt-[2rem]">
+      <Carousel>
+        {profile.map((i) => (
+          <Carousel.Item key={i} interval={4000}>
+            <img
+              className="d-block w-100"
+              style={{ height: "400px", zIndex: 2 }}
+              src={`/carousel_img/${i + 1}.png`}
+              alt="img"
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
 
 export default Carousels;
