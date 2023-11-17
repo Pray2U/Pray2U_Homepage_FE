d50cff6101f6d5cc4ede95377f3f6ba9632c2eea
JanBe <ppk1998@naver.com>
2023-11-18 04:06:21 +0900

    til css change

--- src/components/RegistButton.js
+++ src/components/RegistButton.js
@@ -1,12 +1,16 @@
-import '../styles/Button/RegistButton.scss';
+import "../styles/Button/RegistButton.scss";
 
-const RegistButton = ({onHandleCancel, onHandleSave}) =>{
-    return(
-        <div className="RegistButtons">
-            <div className="RegistCancel" onClick={()=>onHandleCancel()}>취소</div>
-            <div className="RegistSave"  onClick={()=>onHandleSave()}>등록</div>
-        </div>
-    )
-}
+const RegistButton = ({ onHandleCancel, onHandleSave }) => {
+  return (
+    <div className="RegistButtons mb-4">
+      <div className="RegistCancel" onClick={() => onHandleCancel()}>
+        취소
+      </div>
+      <div className="RegistSave" onClick={() => onHandleSave()}>
+        등록
+      </div>
+    </div>
+  );
+};
 
 export default RegistButton;
