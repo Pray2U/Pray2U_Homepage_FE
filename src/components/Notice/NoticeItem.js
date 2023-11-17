6b5e50b09a5007cabae010e8002b644abd757465
JanBe <ppk1998@naver.com>
2023-11-17 10:08:15 +0900

    notice

--- src/components/Notice/NoticeItem.js
+++ src/components/Notice/NoticeItem.js
@@ -15,16 +15,16 @@ const NoticeItem = ({ noticeItem }) => {
       className="w-full h-[14vh] m-auto py-[1%] border-t-[0.1rem] border-t-solid border-t-[#D2D4D9] no-underline text-black cursor-pointer hover:bg-[#eff1f4]"
       onClick={() => moveDetailPage(noticeItem?.postId)}
     >
-      <div className="flex items-center w-full h-[30%]">
+      <div className="flex items-center w-full h-[30%] pl-2">
         <img
           src={noticeItem?.user?.writerProfileImg}
-          className="flex items-center justify-center w-[2rem] h-[2rem] bg-[#C2185B] text-white text-[60%] font-bold rounded-[50%] cursor-pointer no-underline"
+          className="flex items-center justify-center w-[22px] h-[22px] bg-[#C2185B] rounded-[50%] cursor-pointer no-underline"
         />
-        <div className="flex items-center justify-center ml-[0.4rem] h-full font-bold text-[25px]">
+        <div className="flex items-center justify-center ml-[0.4rem] h-full text-[15px]">
           {noticeItem?.user?.writerName}
         </div>
       </div>
-      <div className="flex items-center w-full h-[50%] font-bold text-sm">
+      <div className="flex items-center w-full h-[50%] font-bold text-[25px] pt-2 pl-2">
         {noticeItem?.title}
       </div>
       <div className="flex items-center w-full h-[20%] font-bold ml-auto text-[#374151]">
