9bc0af6c1cff8fc5e5c9a83d464641abe872a9fd
HS980924 <choihs980924@gmail.com>
2023-12-01 14:45:03 +0900

    Add s3 Object delete function and Add object key filter function

--- src/util/s3Upload.js
+++ src/util/s3Upload.js
@@ -1,6 +1,7 @@
 import AWS from 'aws-sdk';
 
 AWS.config.update({
+    region: process.env.REACT_APP_REGION,
     accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
     secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
 });
@@ -11,7 +12,7 @@ export const uploadFile = async(itemImg) => {
     const params = {
         ACL: 'public-read',
         Bucket: process.env.REACT_APP_S3_BUCKET,
-        Key: `${Date.now()}${itemImg?.name}`,
+        Key: `img/${Date.now()}${itemImg?.name}`,
         Body: itemImg,
     };
 
@@ -26,7 +27,7 @@ export const uploadFileList = async(fileList) => {
         const params = {
             ACL: 'public-read',
             Bucket: process.env.REACT_APP_S3_BUCKET,
-            Key: `${Date.now()}_${file?.name}`,
+            Key: `img/${Date.now()}_${file?.name}`,
             Body: file,
         };
         return s3.upload(params).promise();
@@ -38,3 +39,40 @@ export const uploadFileList = async(fileList) => {
     return locations.join(",");
 }
 
+export const deleteFileList = async(keyList) => {
+    try{
+        const s3 = new AWS.S3();
+
+        const objects = keyList.map((key) => {
+            return {Key:key};
+        })
+
+        const params = {
+            Bucket: process.env.REACT_APP_S3_BUCKET, 
+            Delete: {
+                Objects: objects, 
+                Quiet: false
+            }
+        };
+        const result = await s3.deleteObjects(params).promise();
+    }catch(e){
+        alert(e);
+    }
+}
+
+export const extractS3Key = (fileLink) => {
+
+    const fileKey = fileLink.map((file)=>{
+        if(file.includes(process.env.REACT_APP_S3_BUCKET)){
+            const key = file.split('com/');
+            return decodeURI(key[key.length-1]);
+        }
+        return null;
+    });
+
+    if(!fileKey[0]){
+        return null;
+    }
+
+    return fileKey;
+}
