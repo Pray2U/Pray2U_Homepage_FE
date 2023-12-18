84e311350ddd282cf43791749f292e840038a239
HS980924 <choihs980924@gmail.com>
2023-12-18 19:00:13 +0900

    fix ranking page and connect rank api

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -5,7 +5,7 @@ const Title = ({ title, subTitle }) => {
     <div className="flex items-center w-full h-[10vh] m-auto mt-2 rounded-[1rem] bg-[#6495ED] text-white text-[2rem]">
       <div className="flex items-center h-[50%] pl-[3%] font-jua">{title}</div>
       {subTitle ? (
-        <div className="flex items-center h-[40%] pl-[3%] pr-[3%] text-[1.25rem]">
+        <div className="flex items-center h-[40%] pl-[3%] pr-[3%] text-[1.25rem] font-jua">
           {subTitle}
         </div>
       ) : (
--- src/pages/Ranking/Ranking.js
+++ src/pages/Ranking/Ranking.js
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { getCookie, isCheckAdmin } from "../../util/auth";
 
@@ -85,12 +85,13 @@ const Ranking = () => {
   const pageSize = 10;
 
   const [ selectedPage, setSelectedPage ] = useState(1);
-  const [ rankingList, setRankingList ] = useState(data);
+  const [ rankingList, setRankingList ] = useState([]);
   const [ totalItemCnt, setTotalItemCnt ] = useState(1);
+  const [ dateRange, setDataRange] = useState(null);
 
   const read_Ranking = async () => {
     try {
-      const url = `${process.env.REACT_APP_API_SERVER}/api/ranks?page=${selectedPage-1}&size=${pageSize}&sort=id,desc`;
+      const url = `${process.env.REACT_APP_API_SERVER}/api/ranks?page=${selectedPage-1}&size=${pageSize}&sort=id,asc`;
       const response = await axios.get(url, {
         headers: {
           Authorization: `Bearer ${getCookie("accessToken")}`,
@@ -98,9 +99,37 @@ const Ranking = () => {
         withCredentials: true,
       });
       if (response.status === 200) {
-        console.log(response);
-        // setRankingList(response.data.data.content);
-        // setTotalItemCnt(response.data.data.totalElements);
+        const ranking = response.data.data.content
+        setDataRange(ranking[0].startDate.split('T')[0] + " ~ " + ranking[0].endDate.split('T')[0]);
+        const resultList = await Promise.all(
+          ranking.map(async rank => {
+            const result = await read_userInfo(rank?.userId);
+            return Object.assign(rank, result);
+          })
+        );
+        setRankingList(resultList);
+        setTotalItemCnt(response.data.data.totalElements);
+        console.log(resultList);
+      } else {
+        alert(response.data.message);
+      }
+    } catch (e) {
+      alert(e.response.data.message);
+      navigate("/error");
+    }
+  };
+
+  const read_userInfo = async (userId) => {
+    try {
+      const url = `${process.env.REACT_APP_API_SERVER}/api/users/${userId}`;
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${getCookie("accessToken")}`,
+        },
+        withCredentials: true,
+      });
+      if (response.status === 200) {
+        return response.data.data;
       } else {
         alert(response.data.message);
       }
@@ -118,44 +147,45 @@ const Ranking = () => {
   return (
     <>
       <div className="w-[1080px] h-auto m-auto mt-10">
-        <Title title="🏆 랭킹" />
+        <Title title="🏆 랭킹" subTitle={dateRange}/>
         <div className="w-full min-h-[450px] pt-8">
-          <div className="w-[85%] h-[50px] m-auto flex justify-center items-center font-bold mb-5">
-            <div className="flex justify-center items-center w-[15%]">No.</div>
-            <div className="flex justify-center items-center w-[30%]">
-              <div className="w-[50px]">Profile</div>
-              <div className="flex justify-center items-center w-[300px]">Name</div>
+          <div className="w-[90%] h-[50px] m-auto flex justify-center items-center font-bold text-gray-700 text-xl mb-5">
+            <div className="flex justify-center items-center w-[15%] font-jua">No.</div>
+            <div className="flex justify-center items-center w-[30%] font-jua">
+              <div className="flex justify-center items-center w-[70px] font-jua">Profile</div>
+              <div className="flex justify-center items-center w-[300px] font-jua">Name</div>
             </div>
-            <div className="flex justify-center items-center w-[30%]">githubID</div>
-            <div className="flex justify-center items-center w-[25%]">Point</div>
+            <div className="flex justify-center items-center w-[30%] font-jua">githubID</div>
+            <div className="flex justify-center items-center w-[25%] font-jua">Point</div>
           </div>
           {
-            rankingList?.map((d) => (
-              <div key={d.githubId} className="w-[85%] h-[60px] mx-auto flex justify-center items-center font-bold text-xl my-7">
-                <div className="flex justify-center items-center w-[15%]">{d.number}</div>
+            rankingList?.map((rank) => (
+              <div key={rank?.userId} className="w-[90%] h-[80px] mx-auto flex justify-center items-center font-semibold text-xl my-10">
+                <div className="flex justify-center items-center w-[15%] text-2xl font-jua">{rank?.ranking}</div>
                 <div className="flex justify-center items-center w-[30%]">
                   <img
-                    src={`${d.profileimgUrl}`}
-                    className="w-[50px] h-[50px]"
-                    alt="profileImg"
+                    src={`${rank?.profileImgUrl}`}
+                    className="min-w-[70px] h-[70px] rounded-full border-solid border-1 border-gray-500"
+                    alt="profile"
                   />
-                  <div className="flex justify-center items-center w-[300px]">{d.username}</div>
+                  <div className="flex justify-center items-center w-[300px] font-jua">{rank?.username}</div>
                 </div>
-                <div className="flex justify-center items-center w-[30%]">{d.githubId}</div>
-                <div className="flex justify-center items-center w-[25%]">{d.point}pt</div>
+                <Link 
+                  to={`https://github.com/${rank?.githubId}`}
+                  target="_blank" 
+                  className="flex justify-center items-center w-[30%] no-underline font-jua">{rank?.githubId}</Link>
+                <div className="flex justify-center items-center w-[25%] font-jua">{rank?.weekPoint}pt</div>
               </div>
             ))
           }
           {
             totalItemCnt ? 
-            <div className="w-[85%] mx-auto">
-              <Paging
-                pageNum={selectedPage}
-                countPerPage={pageSize}
-                totalItems={totalItemCnt ? totalItemCnt : 0}
-                handlePage={setSelectedPage}
-              />
-            </div> : <></>
+            <Paging
+              pageNum={selectedPage}
+              countPerPage={pageSize}
+              totalItems={totalItemCnt ? totalItemCnt : 0}
+              handlePage={setSelectedPage}
+            /> : <></>
           }
         </div>
       </div>
